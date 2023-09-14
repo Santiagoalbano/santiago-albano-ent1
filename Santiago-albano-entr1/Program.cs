f9f4cac2ba3fb4ac97b8aaf00b789466ea25469d
Santiagolbano <liceo1940@gmail.com>
2023-09-13 21:44:08 -0300

    first commit

---
+++ Santiago-albano-entr1/Program.cs
@@ -0,0 +1,94 @@
+﻿class Program
+{
+    static void Main()
+    { }
+
+
+
+
+public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Mail { get; set; }
+
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contraseña, string mail)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            NombreUsuario = nombreUsuario;
+            Contraseña = contraseña;
+            Mail = mail;
+        }
+    }
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Costo { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public int IdUsuario { get; set; }
+
+        public Producto(int id, string descripcion, decimal costo, decimal precioVenta, int stock, int idUsuario)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            Costo = costo;
+            PrecioVenta = precioVenta;
+            Stock = stock;
+            IdUsuario = idUsuario;
+        }
+    }
+
+    public class ProductoVendido
+    {
+        public int Id { get; set; }
+        public int IdProducto { get; set; }
+        public int Stock { get; set; }
+        public int IdVenta { get; set; }
+
+        public ProductoVendido(int id, int idProducto, int stock, int idVenta)
+        {
+            Id = id;
+            IdProducto = idProducto;
+            Stock = stock;
+            IdVenta = idVenta;
+        }
+    }
+
+    public class Venta
+    {
+        public int Id { get; set; }
+        public string Comentarios { get; set; }
+        public int IdUsuario { get; set; }
+
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            Id = id;
+            Comentarios = comentarios;
+            IdUsuario = idUsuario;
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
